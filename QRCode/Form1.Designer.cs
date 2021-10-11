34390a04a34a6207f43e0d1520c5c46f492e09e1
Guilherme Monteiro <tamandua.0@gmail.com>
2021-10-11 16:29:20 -0300

    Add files via upload

---
+++ QRCode/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace QRCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picQRCode = new System.Windows.Forms.PictureBox();
+            this.textTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGerarQRCode = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQRCode
+            // 
+            this.picQRCode.Location = new System.Drawing.Point(298, 17);
+            this.picQRCode.Name = "picQRCode";
+            this.picQRCode.Size = new System.Drawing.Size(338, 288);
+            this.picQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picQRCode.TabIndex = 0;
+            this.picQRCode.TabStop = false;
+            // 
+            // textTexto
+            // 
+            this.textTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTexto.Location = new System.Drawing.Point(12, 58);
+            this.textTexto.Multiline = true;
+            this.textTexto.Name = "textTexto";
+            this.textTexto.Size = new System.Drawing.Size(259, 26);
+            this.textTexto.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Informe o Texto: ";
+            // 
+            // btnGerarQRCode
+            // 
+            this.btnGerarQRCode.Location = new System.Drawing.Point(67, 122);
+            this.btnGerarQRCode.Name = "btnGerarQRCode";
+            this.btnGerarQRCode.Size = new System.Drawing.Size(135, 66);
+            this.btnGerarQRCode.TabIndex = 7;
+            this.btnGerarQRCode.Text = "Gerar QRCode";
+            this.btnGerarQRCode.UseVisualStyleBackColor = true;
+            this.btnGerarQRCode.Click += new System.EventHandler(this.btnGerarQRCode_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(67, 194);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(135, 33);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpa
+            // 
+            this.btnLimpa.Location = new System.Drawing.Point(67, 233);
+            this.btnLimpa.Name = "btnLimpa";
+            this.btnLimpa.Size = new System.Drawing.Size(135, 33);
+            this.btnLimpa.TabIndex = 9;
+            this.btnLimpa.Text = "Limpar QR";
+            this.btnLimpa.UseVisualStyleBackColor = true;
+            this.btnLimpa.Click += new System.EventHandler(this.btnLimpa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 317);
+            this.Controls.Add(this.btnLimpa);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnGerarQRCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textTexto);
+            this.Controls.Add(this.picQRCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picQRCode;
+        private System.Windows.Forms.TextBox textTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGerarQRCode;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpa;
+    }
+}
+
---
+++ QRCode/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRCoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerarQRCode_Click(object sender, EventArgs e)
+        {
+            if(textTexto.Text == string.Empty)
+            {
+                MessageBox.Show("Campo em branco!");
+                textTexto.Focus();
+                return;
+            }
+
+            try
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(textTexto.Text, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                picQRCode.Image = qrCodeImage;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (picQRCode.Image == null)
+            {
+                MessageBox.Show("Não tem QRCode para salvar.");
+                picQRCode.Focus();
+                return;
+            }
+
+            string Caminho = Application.StartupPath + "\\QRCode.jpg";
+            picQRCode.Image.Save(Caminho);
+            MessageBox.Show("Salvo Com sucesso!");
+        }
+
+        private void btnLimpa_Click(object sender, EventArgs e)
+        {
+            picQRCode.Image = null;
+        }
+    }
+}
